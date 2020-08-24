1675a1c6f44fd51e33c2afc53c26013d3ae8b9ac
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-08-24 19:35:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,11 +23,11 @@ def output_array (array)
 end
 
 def return_string_array (array)
-  counter = 0
+  count = 0
   new_array = []
-  while counter < array.length do
-    new_array.push (array[counter].to_s)
-    count +=1
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
   end
   new_array
 end
