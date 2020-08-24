787caa2100d1e8ae9fa97c96e5fd13cb94d41045
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-08-24 19:20:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,23 @@
 # Write your methods here
+def loop_message_five_times (message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times (message, number)
+
+end
+
+def output_array (array)
+  counter = 0
+
+  while counter < array.length do
+    puts array [counter]
+    counter += 1
+  end
+end
+
+def return_string_array (array)
+
+end
